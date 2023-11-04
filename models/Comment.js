047ad0b5cf30c57cedfff9ab128aa7f309ab1a1e
Jacastro619 <jorgecastro619@gmail.com>
2023-11-03 22:39:42 -0700

    Added the rest of needed models and established relationships in model index.js file

---
+++ models/Comment.js
@@ -0,0 +1,40 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "post",
+        key: "id",
+      },
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "User",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    underscored: true,
+    freezeTableName: true,
+    modelName: "comment",
+  }
+);
