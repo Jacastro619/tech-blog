6ceed24733996cd56e33307b3dfb8d7dcb2ae2ba
Jacastro619 <jorgecastro619@gmail.com>
2023-11-06 15:39:26 -0800

    renamed index.js file to more appropiate name (logout) and added an alert so the user is notified when they have logged out

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,11 +1,11 @@
 const logoutHandler = async () => {
-  console.log("got here");
   const response = await fetch("/api/user/logout", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
   });
 
   if (response.ok) {
+    alert("You have been successfully logged out");
     document.location.replace("/");
   } else {
     alert("Cannot logout");
